c94791ad5aae800af16dac344bee850e48409dbc
AndrewBezold <Andrew.Bezold@gmail.com>
2016-10-13 15:05:59 -0400

    Added functions and variables, along with ToDo list

--- RepPool.sol
+++ RepPool.sol
@@ -6,10 +6,54 @@
     -Distribute earnings
     -Add reporting capability
     -Add ability to retrieve earnings
+    -Add enum results, maybe?
 
 */
 
 
 contract RepPool {
+
+    address creator;
+    address public augur;
+    address[] approved;
+    mapping(address => uint) rep_owned;
+    
+
+    function RepPool(address augurAddress) {
+        creator = msg.sender;
+	augur = augurAddress;
+	approved.push(msg.sender);
+    }
+
+    function depositRep() {
+
+    }
+
+    function withdrawRep() {
+
+    }
+
+    function withdrawRewards() {
+
+    }
+    
+    function report() {
+        //if sender address is in approved address array
+        //use Augur function to report result
+    }
+    
+    function() {
+        //possibly pointless function, only accept REP
+    }
+    
+    !!function setAugurAddress(address newAugur) {
+        if(!!sender == creator.address){
+            augur = newAugur;
+        }
+    }
+    
+    function update {
+    
+    }
 
 }
